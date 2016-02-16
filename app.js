1d40934814c96d9dc03ec80367b1510da51279c9
Student <student@RU-005.local>
2016-02-16 14:48:23 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+// Requires \\
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+// Create Express App Object \\
+var app = express();
+
+// Application Configuration \\
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
+
+// Routes \\
+app.get('/', function(req, res){
+  res.sendFile('index.html', {root : './HTML'})
+});
+
+app.post('/formsubmit', function(req, res){
+	res.redirect('/success')
+})
+
+app.get('/success', function(req, res){
+	res.send('Success!')
+})
+
+// Creating Server and Listening for Connections \\
+var port = 3000
+app.listen(port, function(){
+  console.log('Server running on port ' + port);
+
+})
